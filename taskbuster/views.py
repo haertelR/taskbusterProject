bc4f1872655bb27499bc23cd3052ddcb64d14ea8
RenanHaertel <renanhaertel@hotmail.com>
2018-08-23 15:59:55 -0300

    Part 4 complete

--- taskbuster/views.py
+++ taskbuster/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home(request):
   return render(request, "taskbuster/index.html", {} )
+
+def home_files(request, filename):
+  return render(request, filename, {}, context_type='text/plain')
